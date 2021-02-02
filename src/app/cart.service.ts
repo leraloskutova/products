34bc34ba4df7a1ad096ecc2e26a1a856cc636533
leraloskutova <valeriia.loskutova@yandex.ru>
2021-02-02 19:12:35 +0700

    Дополнение функционала корзины
    - Появилась возможность очищать всю корзину (но еще нельзя удалять какой-то конкретный продукт из корзины).
    - Настроен localStorage. Теперь корзина не сбрасывается после перезагрузки страницы.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,20 +1,66 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart = new BehaviorSubject([]);
   products = [];
+
+  constructor() {
+    this.fillCart();
+  }
+  /**
+   * Проверка на наличие продуктов в корзине.
+   */
+  fillCart() {
+    if (!localStorage.getItem('cart')) {
+      return;
+    }
+
+    var cart = JSON.parse(localStorage.getItem('cart'));
+    this.products = cart;
+    this.cart.next(this.products );
+    return this.cart;
+  }
+  /**
+   * Добавление продукта в корзину.
+   */
   addToCart(product): void {
     this.products.push(product);
+    this.cart.next(this.products);
+    this.setToLocalStorage(product);
   }
-  getProducts() {
-    return this.products;
+  /**
+   * Хранение списка продуктов в корзине между сессиями.
+   */
+  private setToLocalStorage(product?): void {
+    if (!localStorage.getItem('cart')) {
+      localStorage.setItem('cart', JSON.stringify([]));
+    } else {
+      localStorage.setItem('cart', JSON.stringify(this.products));
+      this.cart.next(JSON.parse(localStorage.getItem('cart')));
+    }
+    if (!product) {
+      return;
+    }
   }
+  /**
+   * Очищение корзины.
+   */
   clearCart() {
     this.products = [];
     return this.products;
   }
-  constructor() {}
-
+  /**
+   * Удаление одного продукта в корзине.
+   */
+  // removeFromLocalstorage(id) {
+  //   var cart = JSON.parse(localStorage.getItem('cart'));
+  //   var index = cart.findIndex(x => x.id == id);
+  //
+  //   cart.splice(index, 1); // удаляем с локалсторедж
+  //   localStorage.setItem('cart', JSON.stringify(cart));
+  // }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,20 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from '../cart.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
-  products;
+
+export class CartComponent implements OnInit, OnDestroy {
+  cartList;
+  count;
+  // productIndex: number;
+  // id: number;
+
+  private unsubscribe = new Subject();
+
   constructor(
     private cartService: CartService
-  ) {
-    this.products = this.cartService.getProducts();
-  }
+  ) {}
 
   ngOnInit(): void {
+    this.getCartList();
   }
 
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+  /**
+   * Получение списка продуктов в корзине.
+   */
+  getCartList(): void {
+    this.cartService.cart.pipe(takeUntil(this.unsubscribe))
+      .subscribe(
+        data => {
+          console.log(data);
+          this.cartList = data;
+        });
+    this.checkCart();
+    this.count = this.cartList.length;
+  }
+  /**
+   * Проверка на наличие продуктов в корзине.
+   */
+  checkCart(): void {
+    if (!localStorage.getItem('cart')) {
+      document.getElementById('title-cart').innerHTML = 'Your cart is empty!';
+    }
+  }
+  /**
+   * Очищение корзины.
+   */
+  clearCart(): void {
+    window.alert('Your cart has been cleared!');
+    localStorage.clear();
+    this.cartService.clearCart();
+    window.location.reload();
+  }
+  /**
+   * Удаление одного продукта в корзине.
+   */
+  // deleteCartItem(productId: number) {
+  //   this.cartService.removeFromLocalstorage(productId);
+  //
+  //   var index = this.product.findIndex(x => x.id == productId);
+  //   this.product.splice(index, 1); // удаляем с this.product тоже
+  // }
 }
